0a9d1962e83cee6953a95c52dcc45019df99142a
Sheikh El Moctar El Shengiti <sheikh.el.moctar@gmail.com>
2021-06-16 11:13:27 +0000

    added file java

---
+++ Second/index.java
@@ -0,0 +1,20 @@
+package Second;
+
+/**
+ * index
+ */
+public class index {
+   
+    public static void main(String[] args) {
+        for (int i = 0; i <=5; i++) {
+            for (int k = 1; k <=5-i; k++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <=i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
