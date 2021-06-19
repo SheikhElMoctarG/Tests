9178758c63db12218996998d83b143c710349bf3
Sheikh El Moctar El Shengiti <sheikh.el.moctar@gmail.com>
2021-06-19 16:49:28 +0000

    added file java

---
+++ First-2/First/index.java
@@ -0,0 +1,15 @@
+
+public class index {
+
+    public static void main(String[] args) {
+        for(int i = 0; i <5; i++){
+            for (int j = 0; j <i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k <10; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
