05d10d4ae91145ab7094047fee3efdd010466879
Sheikh El Moctar El Shengiti <sheikh.el.moctar@gmail.com>
2021-06-19 16:57:46 +0000

    added file js

---
+++ First-2/First/index.js
@@ -0,0 +1,12 @@
+for (let i = 0; i <5; i++) {
+    var a = ""
+    var b = ""
+    for (let j = 0; j < i; j++) {
+        a += " "
+    }
+    for (let k = 0; k <10; k++) {
+        b += "*"
+    }
+    console.log(a+b)
+    
+}
